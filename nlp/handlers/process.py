aec4475662d6f6d1d7d7fcdc603305454ad4d695
emilienbuguet <emilien.buguet@epitech.eu>
2022-12-15 15:09:22 +0100

    feat(nlp): clean sentence

--- nlp/handlers/process.py
+++ nlp/handlers/process.py
@@ -1,4 +1,5 @@
 from .request import Req
+from .clean import clean
 
 symptoms = {
     "maux_de_tetes": [ "tête", "tete", "migraine" ],
@@ -9,18 +10,19 @@ symptoms = {
 }
 
 def process(req: Req) -> dict:
+    input = clean(req.input)
     context: list = []
     for symptom in req.symptoms:
-        if "oui" in req.input.lower():
+        if "oui" in input:
             context.append({ "symptom": symptom, "present": True })
-        elif "non" in req.input.lower():
+        elif "non" in input:
             context.append({ "symptom": symptom, "present": False })
         else:
             context.append({ "symptom": symptom, "present": None })
 
     for symptom in symptoms:
         for k in symptoms[symptom]:
-            if k in req.input.lower():
+            if k in input:
                 context.append({ "symptom": symptom, "present": True })
                 break
 
