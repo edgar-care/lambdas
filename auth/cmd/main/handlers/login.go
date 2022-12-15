18fea1e328d085015eb357e083fd9246f5517f5a
ThomasDarrieumerlou <thomas.darrieumerlou@epitech.eu>
2022-12-15 11:45:44 +0100

    feat(format-token): add new format token, now token depend of the type user, doctor or patient

--- auth/cmd/main/handlers/login.go
+++ auth/cmd/main/handlers/login.go
@@ -16,29 +16,37 @@ type loginInput struct {
 
 func Login(w http.ResponseWriter, req *http.Request) {
     var input loginInput
-    var user interface{}
+    var doctor interface{}
+    var patient interface{}
     t := chi.URLParam(req, "type")
+    var token string
 
     err := json.NewDecoder(req.Body).Decode(&input)
     lib.CheckError(err)
 
     if t == "d" {
-        user, err = services.GetDoctorByEmail(input.Email)
+        doctor, err = services.GetDoctorByEmail(input.Email)
     } else {
-        user, err = services.GetPatientByEmail(input.Email)
+        patient, err = services.GetPatientByEmail(input.Email)
     }
 
-    if !(t == "d" && lib.CheckPassword(input.Password, user.(services.Doctor).Password)) &&
-        !(t == "p" && lib.CheckPassword(input.Password, user.(services.Patient).Password)){
+    if !(t == "d" && lib.CheckPassword(input.Password, doctor.(services.Doctor).Password)) &&
+        !(t == "p" && lib.CheckPassword(input.Password, patient.(services.Patient).Password)){
         lib.WriteResponse(w, map[string]string{
             "message": "Username and password mismatch.",
             }, 400)
         return
     }
 
-    token, err := lib.CreateToken(map[string]interface{}{
-        "user": user,
-    })
+    if (t == "d") {
+        token, err = lib.CreateToken(map[string]interface{}{
+            "doctor": doctor,
+        })
+    } else {
+        token, err = lib.CreateToken(map[string]interface{}{
+            "patient": patient,
+        })
+    }
 
     lib.CheckError(err)
     lib.WriteResponse(w, map[string]interface{}{
