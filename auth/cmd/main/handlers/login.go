1f01eab430ff9f76c630c776488ca16adbf622cf
lcox <72014202+Lucas-COX@users.noreply.github.com>
2022-12-15 14:49:15 +0100

    fix(auth): detail error on bad request' (#18)

--- auth/cmd/main/handlers/login.go
+++ auth/cmd/main/handlers/login.go
@@ -1,55 +1,60 @@
 package handlers
 
 import (
-    "encoding/json"
-    //"github.com/davecgh/go-spew/spew"
-    "github.com/edgar-care/auth/cmd/main/lib"
-    "github.com/edgar-care/auth/cmd/main/services"
-    "github.com/go-chi/chi/v5"
-    "net/http"
+	"encoding/json"
+	"net/http"
+
+	"github.com/edgar-care/auth/cmd/main/lib"
+	"github.com/edgar-care/auth/cmd/main/services"
+	"github.com/go-chi/chi/v5"
 )
 
 type loginInput struct {
-    Email       string `json:"email"`
-    Password    string `json:"password"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func Login(w http.ResponseWriter, req *http.Request) {
-    var input loginInput
-    var doctor interface{}
-    var patient interface{}
-    t := chi.URLParam(req, "type")
-    var token string
+	var input loginInput
+	var doctor interface{}
+	var patient interface{}
+	t := chi.URLParam(req, "type")
+	var token string
 
-    err := json.NewDecoder(req.Body).Decode(&input)
-    lib.CheckError(err)
+	err := json.NewDecoder(req.Body).Decode(&input)
+	lib.CheckError(err)
 
-    if t == "d" {
-        doctor, err = services.GetDoctorByEmail(input.Email)
-    } else {
-        patient, err = services.GetPatientByEmail(input.Email)
-    }
+	if t == "d" {
+		doctor, err = services.GetDoctorByEmail(input.Email)
+	} else {
+		patient, err = services.GetPatientByEmail(input.Email)
+	}
+	if err != nil {
+		lib.WriteResponse(w, map[string]string{
+			"message": "Could not find user: " + err.Error(),
+		}, http.StatusBadRequest)
+	}
 
-    if !(t == "d" && lib.CheckPassword(input.Password, doctor.(services.Doctor).Password)) &&
-        !(t == "p" && lib.CheckPassword(input.Password, patient.(services.Patient).Password)){
-        lib.WriteResponse(w, map[string]string{
-            "message": "Username and password mismatch.",
-            }, 400)
-        return
-    }
+	if !(t == "d" && lib.CheckPassword(input.Password, doctor.(services.Doctor).Password)) &&
+		!(t == "p" && lib.CheckPassword(input.Password, patient.(services.Patient).Password)) {
+		lib.WriteResponse(w, map[string]string{
+			"message": "Username and password mismatch.",
+		}, http.StatusBadRequest)
+		return
+	}
 
-    if (t == "d") {
-        token, err = lib.CreateToken(map[string]interface{}{
-            "doctor": doctor,
-        })
-    } else {
-        token, err = lib.CreateToken(map[string]interface{}{
-            "patient": patient,
-        })
-    }
+	if t == "d" {
+		token, err = lib.CreateToken(map[string]interface{}{
+			"doctor": doctor,
+		})
+	} else {
+		token, err = lib.CreateToken(map[string]interface{}{
+			"patient": patient,
+		})
+	}
 
-    lib.CheckError(err)
-    lib.WriteResponse(w, map[string]interface{}{
-        "token": token,
-    }, 200)
+	lib.CheckError(err)
+	lib.WriteResponse(w, map[string]interface{}{
+		"token": token,
+	}, http.StatusOK)
 }
