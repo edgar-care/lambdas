280e59564ac8648fd8b65447238bd8f0c238dbf9
emilienbuguet <72021370+emilienbuguet@users.noreply.github.com>
2023-10-25 18:32:13 +0200

    feat(algo): add coverage algorithm (#49)

--- exam/cmd/main/services/graphql.go
+++ exam/cmd/main/services/graphql.go
@@ -2,8 +2,8 @@ package services
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/jinzhu/copier"
 	"os"
 
 	"github.com/machinebox/graphql"
@@ -11,49 +11,92 @@ import (
 
 /********** Types ***********/
 
+type DiseasesResponse struct {
+	GetDiseases []Disease `json:"getDiseases"`
+}
+
 type Disease struct {
 	Id       string   `json:"id"`
 	Code     string   `json:"code"`
 	Name     string   `json:"name"`
 	Symptoms []string `json:"symptoms"`
-	Advice   *string  `json:"advice"`
-}
-
-type DiseaseOutput struct {
-	Id       string    `json:"id"`
-	Code     *string   `json:"code"`
-	Name     *string   `json:"name"`
-	Symptoms *[]string `json:"symptoms"`
-	Advice   *string   `json:"advice"`
-}
-
-/**************** GraphQL types *****************/
-
-type getDiseasesResponse struct {
-	Content []DiseaseOutput `json:"getDiseases"`
+	Advice   string   `json:"advice"`
 }
 
 /*************** Implementations *****************/
 
 func GetDiseases() ([]Disease, error) {
-	var diseases getDiseasesResponse
-	var resp []Disease
-	query := `query getDiseases {
-                getDiseases {
-                    id
-                    code
-                    name
-                    symptoms
-                    advice
-                }
-            }`
-	err := Query(query, map[string]interface{}{}, &diseases)
-	if err != nil {
-		panic(err)
+	var resp DiseasesResponse
+	var err error
+
+	jsonData := `{
+		"getDiseases": [
+			{
+				"id": "65390b614c208161d18037ce",
+				"code": "asthme",
+				"name": "Asthme",
+				"symptoms": [
+					"respiration_difficile",
+					"toux",
+					"respiration_sifflante",
+					"somnolence",
+					"anxiete"
+				],
+				"advice": null
+			},
+			{
+				"id": "65390c784c208161d18037cf",
+				"code": "brulure_estomac",
+				"name": "Brulure d'estomac",
+				"symptoms": [
+					"brulure_poitrine",
+					"respiration_difficile",
+					"boule_gorge",
+					"toux"
+				],
+				"advice": null
+			},
+			{
+				"id": "65392a0c467ff3023b4631f1",
+				"code": "migraine_aura",
+				"name": "Migraine avec aura",
+				"symptoms": [
+					"maux_de_tetes",
+					"vision_trouble",
+					"tache_visuel"
+				],
+				"advice": null
+			},
+			{
+				"id": "65392a500ef9f8b48f6e0a7a",
+				"code": "migraine_sans_aura",
+				"name": "Migraine sans aura",
+				"symptoms": [
+					"maux_de_tetes",
+					"douleur_pulsatile",
+					"vomissements",
+					"sensibilite_lumiere"
+				],
+				"advice": null
+			},
+			{
+				"id": "65392a780ef9f8b48f6e0a7b",
+				"code": "migraine_vestibulaire",
+				"name": "Migraine vestibulaire",
+				"symptoms": [
+					"maux_de_tetes",
+					"vertige",
+					"perte_equilibre"
+				],
+				"advice": null
+			}
+		]
+	}`
+
+	if err = json.Unmarshal([]byte(jsonData), &resp); err != nil {
+		fmt.Println("Error:", err)
 	}
-	fmt.Print(diseases)
-	_ = copier.Copy(&resp, &diseases.Content)
-	return resp, err
+	return resp.GetDiseases, err
 }
 
 func createClient() *graphql.Client {
