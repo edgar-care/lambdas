f83b0955e4745415f9f57e762c369ab9b220edb7
LohicM <loic.marlard@epitech.eu>
2024-02-14 09:36:19 +0100

    change Antedisease

--- graphql/cmd/main/database/models/antedisease.go
+++ graphql/cmd/main/database/models/antedisease.go
@@ -5,27 +5,30 @@ import (
 )
 
 type AnteDisease struct {
-	ID         primitive.ObjectID `bson:"_id"`
-	Name       string             `bson:"name,omitempty"`
-	Chronicity float64            `bson:"chronicity,omitempty"`
-	Chir       *string            `bson:"chir,omitempty"`
-	Treatment  *[]string          `bson:"treatment,omitempty"`
-	Symptoms   *[]string          `bson:"symptoms,omitempty"`
+	ID            primitive.ObjectID `bson:"_id"`
+	Name          string             `bson:"name,omitempty"`
+	Chronicity    float64            `bson:"chronicity,omitempty"`
+	SurgeryIds    *[]string          `bson:"surgery_ids,omitempty"`
+	Symptoms      *[]string          `bson:"symptoms,omitempty"`
+	TreatmentIds  *[]string          `bson:"treatment_ids,omitempty"`
+	StillRelevant bool               `bson:"still_relevant,omitempty"`
 }
 
 type AnteDiseaseCreateInput struct {
-	Name       string    `bson:"name,omitempty"`
-	Chronicity float64   `bson:"chronicity,omitempty"`
-	Chir       *string   `bson:"chir,omitempty"`
-	Treatment  *[]string `bson:"treatment,omitempty"`
-	Symptoms   *[]string `bson:"symptoms,omitempty"`
+	Name          string    `bson:"name,omitempty"`
+	Chronicity    float64   `bson:"chronicity,omitempty"`
+	SurgeryIds    *[]string `bson:"surgery_ids,omitempty"`
+	Symptoms      *[]string `bson:"symptoms,omitempty"`
+	TreatmentIds  *[]string `bson:"treatment_ids,omitempty"`
+	StillRelevant bool      `bson:"still_relevant,omitempty"`
 }
 
 type AnteDiseaseUpdateInput struct {
-	ID         string    `bson:"_id"`
-	Name       *string   `bson:"name,omitempty"`
-	Chronicity *float64  `bson:"chronicity,omitempty"`
-	Chir       *string   `bson:"chir,omitempty"`
-	Treatment  *[]string `bson:"treatment,omitempty"`
-	Symptoms   *[]string `bson:"symptoms,omitempty"`
+	ID            string    `bson:"_id"`
+	Name          *string   `bson:"name,omitempty"`
+	Chronicity    *float64  `bson:"chronicity,omitempty"`
+	SurgeryIds    *[]string `bson:"surgery_ids,omitempty"`
+	Symptoms      *[]string `bson:"symptoms,omitempty"`
+	TreatmentIds  *[]string `bson:"treatment_ids,omitempty"`
+	StillRelevant *bool     `bson:"still_relevant,omitempty"`
 }
